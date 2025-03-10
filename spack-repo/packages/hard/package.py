99f38cd39652bd15af49655bcdef5cf2c3a23ad2
Julien Loiseau <jloiseau@lanl.gov>
2025-03-10 15:28:21 -0600

    Fix spack repo

--- spack-repo/packages/hard/package.py
+++ spack-repo/packages/hard/package.py
@@ -17,7 +17,7 @@ class Hard(CMakePackage):
     variant("radiation", default=True, description="Enable support for radiation physics")
     variant("tests", default=False, description="Enable unit tests")
 
-    depends_on("flecsi@2.3.0:")
+    depends_on("flecsi@2.3.0: +flog")
     depends_on("libcatalyst", when="+catalyst")
     depends_on("yaml-cpp@0.8:")
     depends_on("singularity-eos@main +hdf5 +spiner +eospac build_extra=sesame")
