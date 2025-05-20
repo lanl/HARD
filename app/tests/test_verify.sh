674edc7d6c5ac9a0b2c74f1f51662fa57e53e43e
Julien Loiseau <jloiseau@lanl.gov>
2025-05-19 18:02:51 -0600

    Add check for tests on hydro only

--- app/tests/test_verify.sh
+++ app/tests/test_verify.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 # Path to the executable
 mpi_executable="$1"
@@ -6,10 +7,7 @@ hard_executable="$2"
 config_file="$3"
 additional_args="$4"
 
-$mpi_executable -np 1 $hard_executable $config_file $additional_args
-make verify CONFIG=$config_file
+$mpi_executable -np 1 $hard_executable $config_file $additional_args && \
+  make verify CONFIG=$config_file
 rm -rf *.raw
-
-# Exit with the status from diff (0 for no differences, 1 for differences)
-exit $exit_status
 
