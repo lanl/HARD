bb105116edf39b1b966285b24bb9521d32f7a32d
Julien Loiseau <jloiseau@lanl.gov>
2026-04-03 13:01:48 -0600

    Fix cuda for Flecsolve

--- spack-repo/packages/flecsolve/package.py
+++ spack-repo/packages/flecsolve/package.py
@@ -7,28 +7,30 @@ from spack.package import *
 class Flecsolve(CMakePackage, CudaPackage, ROCmPackage):
     """Solvers package built on top of FleCSI"""
 
-    homepage="https://github.com/lanl/flecsolve.git"
+    homepage = "https://github.com/lanl/flecsolve.git"
     git = "https://github.com/lanl/flecsolve.git"
-    # TO DO update to stable when available
+
     version("main", commit="661877df93b594e2447790fc313cca4446837fad")
 
     variant("tests", default=False, description="Enable unit tests")
     variant("standard", default=False, description="Standard setup for flecsolve")
 
-    depends_on('flecsi@2.4:')
+    depends_on("flecsi@2.4:")
+    depends_on("eigen")
     depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("fortran", type="build")
-    depends_on('eigen')
-    #depends_on('amp+hypre', when="+standard") # Might want to enable at some point.
-    #depends_on('stacktrace+shared', when="+standard")
-    #depends_on('lapackwrappers@main', when="+standard")
+
+    depends_on("cuda", when="+cuda")
 
     def cmake_args(self):
         args = [
             self.define_from_variant("FLECSOLVE_ENABLE_UNIT_TESTS", "tests"),
             self.define_from_variant("FLECSOLVE_ENABLE_AMP", "standard"),
         ]
+
+        if "+cuda" in self.spec:
+            cuda_prefix = self.spec["cuda"].prefix
+            args.append(self.define("CMAKE_CXX_FLAGS", f"--cuda-path={cuda_prefix}"))
+
         return args
-
-
