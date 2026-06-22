d7e9affb4dc1878e89913190b3b57990ea706b02
yrasool <111371895+yrasool@users.noreply.github.com>
2026-06-22 13:27:12 -0400

    Fix flecsolve Spack branch

--- spack-repo/packages/flecsolve/package.py
+++ spack-repo/packages/flecsolve/package.py
@@ -10,7 +10,7 @@ class Flecsolve(CMakePackage):
     homepage="https://github.com/lanl/flecsolve.git"
     git = "https://github.com/lanl/flecsolve.git"
     # TO DO update to stable when available
-    version("main", branch="flecsi-2.4")
+    version("main", branch="main")
 
     variant("tests", default=False, description="Enable unit tests")
     variant("standard", default=False, description="Standard setup for flecsolve")
